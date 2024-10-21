07a7295a7eb099b1e53668f012d9bdcc4d63f9ac
Paulramirezz <ramirezjohnpaul747@gmail.com>
2024-10-21 10:05:05 +0800

    Create object.Activity.js

---
+++ object.Activity.js
@@ -0,0 +1,14 @@
+let car = {
+    type: "Ferrari",
+    model: "Daytona SP3",
+    color: "Red
+"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
